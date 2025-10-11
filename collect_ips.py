1841ae54fa24cad82ba63bd76a31e0a099cda481
Msii6 <93140793+Msii6@users.noreply.github.com>
2025-10-12 02:01:01 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,11 +5,7 @@ import os
 
 # 目标URL列表
 urls = [
-    'https://ip.164746.xyz/ipTop10.html',
-    'https://cf.090227.xyz',
     'https://api.uouin.com/cloudflare.html',
-    'https://www.wetest.vip/page/cloudflare/address_v4.html',
-    'https://stock.hostmonit.com/CloudFlareYes'
 ]
 
 # 正则表达式用于匹配IP地址
